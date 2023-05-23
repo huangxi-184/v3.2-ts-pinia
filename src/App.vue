2e60563111d18c6132fb79e47fa503278239a0df
haungxi <1842196429@qq.com>
2023-05-23 22:17:26 +0800

    feat: 提交新的配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
 
 <template>
   <div>
-    <h1>wishing</h1>
+    <el-button type="danger" round>Danger</el-button>
+    <svg-icon name="phone" width="400px" height="400px"></svg-icon>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-const fun = () => {
-  console.log('test')
-}
+const app = createApp(App)
 
-fun()
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+// svg 配置代码
+import 'virtual:svg-icons-register'
+
+app.mount('#app')
